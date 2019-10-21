13d12bb84df59967f882a03afd3c383420b20355
tjclawson <tjamesclawson@gmail.com>
2019-10-21 14:20:51 -0400

    Add MainActivity and Drawer Layout
    Co-authored-by: justinbgent <justinbgent@gmail.com>

--- app/src/main/java/com/example/receipttracker/MainActivity.kt
+++ app/src/main/java/com/example/receipttracker/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.receipttracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = title
+
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+
+            when(menuItem.itemId) {
+
+                R.id.nav_category_first -> { Toast.makeText(this, "First", Toast.LENGTH_LONG).show() }
+                R.id.nav_category_second -> { Toast.makeText(this, "Second", Toast.LENGTH_LONG).show() }
+                R.id.nav_category_third -> { Toast.makeText(this, "Third", Toast.LENGTH_LONG).show() }
+            }
+
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 }
