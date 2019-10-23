bb4513d3884df539de693a53f5247f4f72c4cfe0
tjclawson <tjamesclawson@gmail.com>
2019-10-23 18:27:18 -0400

    Add ability to add and edit photo and upload to cloudinary and post URL to api

--- app/src/main/java/com/example/receipttracker/EditReceiptFragment.kt
+++ app/src/main/java/com/example/receipttracker/EditReceiptFragment.kt
@@ -1,16 +1,21 @@
 package com.example.receipttracker
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.example.receipttracker.model.Receipt
+import com.example.receipttracker.view.AddReceiptFragment
 import com.example.receipttracker.view.MyReceiptsFragment
+import com.example.receipttracker.viewmodel.AddEditReceiptViewModel
 import kotlinx.android.synthetic.main.add_receipt_fragment.*
 import kotlinx.android.synthetic.main.edit_receipt_fragment.*
 
@@ -19,9 +24,7 @@ class EditReceiptFragment(receipt: Receipt) : Fragment() {
 
     val oldReceipt = receipt
 
-
-
-    private lateinit var viewModel: EditReceiptViewModel
+    private lateinit var viewModel: AddEditReceiptViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,18 +33,29 @@ class EditReceiptFragment(receipt: Receipt) : Fragment() {
         return inflater.inflate(R.layout.edit_receipt_fragment, container, false)
     }
 
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(EditReceiptViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(AddEditReceiptViewModel::class.java)
 
-        //oldReceipt = arguments?.getSerializable("RECEIPT") as Receipt?
-        Log.i("BIGBRAIN", oldReceipt?.amount_spent ?: "null af")
         et_edit_amount_spent.setText(oldReceipt?.amount_spent)
         et_edit_date_of_transaction.setText(oldReceipt?.date_of_transaction)
         et_edit_description.setText(oldReceipt?.description)
         et_edit_merchant.setText(oldReceipt?.merchant)
         et_edit_category.setText(oldReceipt?.category)
 
+        if(oldReceipt.image_url != null) {
+            Glide.with(this.activity!!.applicationContext)
+                .load(oldReceipt.image_url)
+                .into(iv_edit_receipt_photo)
+        }
+
+        iv_edit_receipt_photo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, AddReceiptFragment.IMAGE_REQUEST_CODE)
+        }
 
         button_save_receipt.setOnClickListener {
             val newReceipt = Receipt(
@@ -49,14 +63,13 @@ class EditReceiptFragment(receipt: Receipt) : Fragment() {
                 et_edit_date_of_transaction.text.toString(),
                 et_edit_category.text.toString(),
                 et_edit_merchant.text.toString(),
-                "",
+                oldReceipt.image_url ?: "",
                 viewModel.repo?.currentUser!!,
                 et_edit_description.text.toString(),
                 null
             )
 
             if(oldReceipt.id != null ) {
-
 
                 viewModel.editReceipt(viewModel.repo?.currentToken!!, oldReceipt.id!!, newReceipt)
                     ?.observe(this, Observer {
@@ -72,4 +85,20 @@ class EditReceiptFragment(receipt: Receipt) : Fragment() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AddReceiptFragment.IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val photoUri: Uri? = data?.data
+            if (photoUri != null) {
+                iv_edit_receipt_photo.setImageURI(Uri.parse(photoUri.toString()))
+                viewModel.uploadReceiptPhoto(photoUri)?.observe(this, Observer {
+                    if (it != AddEditReceiptViewModel.WAIT_KEY) {
+                        if (it.isNotBlank()) {
+                            oldReceipt.image_url = it
+                        } else Toast.makeText(this.context, "Failed to upload image", Toast.LENGTH_LONG).show()
+                    }
+                })
+            }
+        }
+    }
 }
